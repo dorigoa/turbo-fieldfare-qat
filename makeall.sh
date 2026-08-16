313a80e8a1303dc075049c8c986a9d590b92021a
dorigo_a <alvise.dorigo@psi.ch>
2026-08-16 11:11:30 +0200

    fix

--- makeall.sh
+++ makeall.sh
@@ -1,6 +1,14 @@
-#git clone git@github.com:dorigoa/turbo-fieldfare-fork.git
+#!/usr/bin/env bash
+
 git clone git@github.com:drumih/turbo-fieldfare.git
-python3 ./pin_model.py --repo-path ./turbo-fieldfare-fork/
+
+python3 ./pin_model.py --repo-path ./turbo-fieldfare/
+rc=$?
+if [ "$rc" -ne 0 ]; then
+    echo "ERRORE: pin_model.py terminated with code $rc. Stop." >&2
+    return "$rc" 2>/dev/null || exit "$rc" #exit 1
+fi
+
 cd turbo-fieldfare
 mkdir -p Scratch
 cp ../build-app.sh Scratch/
