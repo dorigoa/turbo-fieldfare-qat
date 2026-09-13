5b993aa35183843eab9a17175a6392d078ce4840
dorigoa <alvise72@live.com>
2026-09-13 12:28:40 +0200

    Uncomment patch.sh and update sed command

--- makeall.sh
+++ makeall.sh
@@ -2,7 +2,7 @@
 
 git clone git@github.com:drumih/turbo-fieldfare.git
 
-#./patch.sh
+./patch.sh
 #rc=$?
 #if [ "$rc" -ne 0 ]; then
 #    echo "ERRORE: pin_model.py terminated with code $rc. Stop." >&2
@@ -11,7 +11,7 @@ git clone git@github.com:drumih/turbo-fieldfare.git
 
 cd turbo-fieldfare
 
-#sed -i '' 's+Text("Gemma 4 26B")+Text("Gemma 4 26B A4B IT QAT Q4")+' Sources/TurboFieldfareApp/Mac/Components/ModelStatusBadge.swift
+sed -i '' 's+Text("Gemma 4 26B")+Text("Gemma 4 26B A4B IT QAT Q4")+' Sources/TurboFieldfareApp/Mac/Components/ModelStatusBadge.swift
 
 mkdir -p Scratch
 cp ../build-app.sh Scratch/
