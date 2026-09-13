c50ddb18c0a08da41048d92baab8bf3a43f9e020
dorigo_a <alvise.dorigo@psi.ch>
2026-09-13 13:48:05 +0200

    fix

--- makeall.sh
+++ makeall.sh
@@ -2,16 +2,24 @@
 
 git clone git@github.com:drumih/turbo-fieldfare.git
 
-./patch.sh
-#rc=$?
-#if [ "$rc" -ne 0 ]; then
-#    echo "ERRORE: pin_model.py terminated with code $rc. Stop." >&2
-#    return "$rc" 2>/dev/null || exit "$rc" #exit 1
-#fi
+if [[ -z "${1:-}" ]]; then
+    echo "Error: argument with quantization number is mandatory ('4', '5', '6', '8')." >&2
+    exit 1
+fi
+
+case "$1" in
+    4|5|6|8) ;;
+    *)
+        echo "Error: invalid quantization '$1' (allowed: '4', '5', '6', '8')." >&2
+        exit 1
+        ;;
+esac
+
+./patch.sh $1
 
 cd turbo-fieldfare
 
-sed -i '' 's+Text("Gemma 4 26B")+Text("Gemma 4 26B A4B IT QAT Q4")+' Sources/TurboFieldfareApp/Mac/Components/ModelStatusBadge.swift
+sed -i '' 's+Text("Gemma 4 26B")+Text("Gemma 4 26B A4B IT QAT Q$1")+' Sources/TurboFieldfareApp/Mac/Components/ModelStatusBadge.swift
 
 mkdir -p Scratch
 cp ../build-app.sh Scratch/
